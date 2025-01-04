bfc9a132d13053f208dbeb9515d16d2a24ba4715
r. ávila <avila.dev@outlook.com>
2025-01-04 16:59:07 -0300

    feat: add superuser environments

--- env.go
+++ env.go
@@ -10,13 +10,20 @@ type envs struct {
 	ServerURL string `env:"SERVER_URL,default=localhost:8090"`
 
 	PocketBase struct {
+		Admin struct {
+			Email    string `env:"PB_ADMIN_EMAIL,default=admin@default.com"`
+			Password string `env:"PB_ADMIN_PASSWORD,default=1234567890"`
+		}
+
 		DebugMode bool   `env:"PB_DEBUG_MODE,default=true"`
 		PBDataDir string `env:"PB_DATA_DIR,default=./pb_data"`
 	}
 }
 
 var Config = func() *envs {
-	env.Load(RootPath) // Optional error handling
+	// Error handling isn't mandatory because the .env file may already be loaded by the
+	// container, e.g., via Docker Compose's `env_file` directive or similar mechanisms.
+	env.Load(RootPath)
 
 	envs := envs{}
 	if err := env.Decode(&envs); err != nil {
